cf85e9626a9c2384d9a26d9e5ae24c7723d69318
MichiBaum <christiangargamel@gmail.com>
2020-03-16 09:42:52 +0100

    basic initialize
    basic initialize

---
+++ src/main/java/com/itensis/ecat/ECatApplication.java
@@ -0,0 +1,21 @@
+package com.itensis.ecat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@SpringBootApplication
+@PropertySources({
+		@PropertySource("classpath:application.properties"),
+		@PropertySource("classpath:email.properties")
+})
+public class ECatApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args){
+		SpringApplication.run(ECatApplication.class, args);
+	}
+
+
+}
