3a7b5809bc9642bd358df1464230112342ce7091
MichiBaum <christiangargamel@gmail.com>
2020-03-18 07:14:00 +0100

    Create ProductGroup.java
    #36

---
+++ src/main/java/com/itensis/ecat/domain/ProductGroup.java
@@ -0,0 +1,22 @@
+package com.itensis.ecat.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+public class ProductGroup extends AbstractEntity {
+
+	@Column(nullable = false)
+	private String name;
+
+	@OneToMany
+	private List<ProductClass> productClasses;
+
+}
