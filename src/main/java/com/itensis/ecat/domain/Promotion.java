08756f85a15a70d27e4c7ab34d38d881c0dbd121
MichiBaum <christiangargamel@gmail.com>
2020-03-18 10:43:00 +0100

    Add Promotion
    #30

---
+++ src/main/java/com/itensis/ecat/domain/Promotion.java
@@ -0,0 +1,31 @@
+package com.itensis.ecat.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "PROMOTION")
+public class Promotion extends AbstractEntity {
+
+	@Column(nullable = false, name = "TITLE")
+	private String title;
+
+	@Column(nullable = false, name = "DESCRIPTION")
+	private String description;
+
+	@Column(nullable = false, name = "START_DATE")
+	private Long startDate;
+
+	@Column(nullable = false, name = "END_DATE")
+	private Long endDate;
+
+	@Column(nullable = false, name = "CREATION_DATE")
+	private Long creationDate;
+
+}
---
+++ src/main/java/com/itensis/ecat/repository/PromotionRepository.java
@@ -0,0 +1,9 @@
+package com.itensis.ecat.repository;
+
+import com.itensis.ecat.domain.Promotion;
+
+public interface PromotionRepository extends CustomJpaRepository<Promotion, Long> {
+
+
+
+}
